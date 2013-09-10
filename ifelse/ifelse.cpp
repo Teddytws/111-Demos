fdbb85ad49c05a0c026942114bee4574a80a3082
Teddytws <teddy.tws@gmail.com>
2013-09-10 15:50:10 -0700

    reset for new semester

--- ifelse/ifelse.cpp
+++ ifelse/ifelse.cpp
@@ -5,28 +5,6 @@ using namespace std;
 
 int main () {
 
-	string light;
-	string direction;
-
-	cout << "what color is the light? ";
-	cin >> light;
-
-	cout << "what direction are you going? ";
-	cin >> direction;
-
-	if (light == "red") {
-	//red light code goes here
-		if (direction == "right") {
-			cout<<"stop, then go"<<endl;
-		}
-		else {
-			cout<<"stop"<<endl;
-		}
-	}
-	else {
-		cout<<"go"<<endl;
-	}
-
 
 	return 0;
 }
