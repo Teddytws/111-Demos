f3d2881314c340ec515175a25c2e4d885dac62b3
Teddytws <teddy.tws@gmail.com>
2013-09-10 17:40:17 -0700

    add ifelse demo

--- ifelse/ifelse.cpp
+++ ifelse/ifelse.cpp
@@ -5,6 +5,34 @@ using namespace std;
 
 int main () {
 
+	int temp;
+	int pressure;
+
+	cout << "what is the temp?: ";
+	cin >> temp;
+	cout << "what is the pressure: ";
+	cin >> pressure;
+
+	if (temp > 100 && pressure > 100)
+	{
+		cout << "shutdown" << endl;
+	}
+
+	else if (temp > 100 || pressure > 100)
+	{
+		cout << "warning" << endl;
+	}
+
+	else {
+		cout << "OK" << endl;
+	}
+
+
 
 	return 0;
 }
+
+
+
+
+
